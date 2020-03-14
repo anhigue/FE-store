9480f49acf655539262e84290b01a1a9977cdaed
Andres Higueros <higueros71@gmail.com>
2020-03-14 01:10:08 -0600

    agregando funcionalidad completa para poder eliminar y agregar un vehiculo para la lista de repuestos. Agregando una interface universal para poder definir acciones dentro de un dialog. Agregando nueva funcion dentro del servicio de dialogos para no repetir codigo dentro de los componentes

--- src/app/services/dialog/dialog.service.ts
+++ src/app/services/dialog/dialog.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { ComponentType } from '@angular/cdk/portal';
+import { DialogCustomComponent } from '../../components/dialog-custom/dialog-custom.component';
 
 @Injectable({
   providedIn: 'root'
@@ -13,13 +14,21 @@ export class DialogService {
 
   constructor(private dialog: MatDialog) {}
 
-  openDialog(comp: ComponentType<unknown>): MatDialogRef<unknown> {
+  public openDialog(comp: ComponentType<unknown>): MatDialogRef<unknown> {
     const dialogRef = this.dialog.open(comp, {
       width: this.width,
       height: this.height,
       data: this.shareData
     });
-
     return dialogRef;
+  }
+
+  public errorMessage(error: any, title: string, message: string) {
+    this.shareData = {
+      title,
+      message,
+      data: error
+    };
+    this.openDialog(DialogCustomComponent);
   }
 }
