377b7ba9c01e896caa940afa5f26f9e2b7559572
Andres Higueros <higueros71@gmail.com>
2020-04-03 15:19:55 -0600

    cambiando la direccion ip de donde sera corrido el sistema de ventas de repuestos

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_BASE: 'http://localhost:8080',
+  API_BASE: 'http://192.168.56.105:8080',
   KEY_JSON: 'SIE_PASS',
   algorithm: 'aes256',
   encoding: 'hex',
