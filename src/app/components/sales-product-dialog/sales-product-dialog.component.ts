4d18d09e6155fbc394b18e2d5c00607c575d8052
Andres Higueros <higueros71@gmail.com>
2020-04-18 23:59:10 -0600

    agregando filtro dinamico por vehiculo

--- src/app/components/sales-product-dialog/sales-product-dialog.component.ts
+++ src/app/components/sales-product-dialog/sales-product-dialog.component.ts
@@ -1,3 +1,4 @@
+import { VehicleInterface } from './../../../interfaces/VehicleInterface';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { ProductInterface } from '../../../interfaces/ProductInterface';
@@ -9,7 +10,7 @@ import { MatSort } from '@angular/material/sort';
 @Component({
   selector: 'app-sales-product-dialog',
   templateUrl: './sales-product-dialog.component.html',
-  styleUrls: ['./sales-product-dialog.component.scss']
+  styleUrls: ['./sales-product-dialog.component.scss'],
 })
 export class SalesProductDialogComponent implements OnInit {
   displayedColumns: string[] = [
@@ -19,7 +20,7 @@ export class SalesProductDialogComponent implements OnInit {
     'partNo',
     'price',
     'stock',
-    'options'
+    'options',
   ];
   products: ProductInterface[] = [];
   productsFabric: ProductInterface[] = [];
@@ -27,13 +28,16 @@ export class SalesProductDialogComponent implements OnInit {
   dataSourceFabric: MatTableDataSource<ProductInterface>;
   cars = ELEMENT_DATA_VEHICLES;
 
+  vehicleSelected: VehicleInterface;
+  partsFound: ProductInterface[] = [];
+
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
-  @ViewChild(MatSort, {static: true}) sort: MatSort;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
 
   constructor(
     private _DIALOG_SERVICE: DialogService,
     private _PRODUCT_SERVICE: ProductService
-  ) { }
+  ) {}
 
   ngOnInit() {
     this.getProduct();
@@ -43,20 +47,39 @@ export class SalesProductDialogComponent implements OnInit {
   private getProduct(): void {
     try {
       /* descomenta estas lineas cuando termines de agregar las rutas */
-      this._PRODUCT_SERVICE.readProduct().subscribe( (value: ProductInterface[]) => {
-        if (value) {
-          this.products = value;
-          this.dataSource = new MatTableDataSource<ProductInterface>(this.products);
-          this.dataSource.paginator = this.paginator;
-          this.dataSource.sort = this.sort;
-        }
-      });
+      this._PRODUCT_SERVICE
+        .readProduct()
+        .subscribe((value: ProductInterface[]) => {
+          if (value) {
+            this.products = value;
+            this.dataSource = new MatTableDataSource<ProductInterface>(
+              this.products
+            );
+            this.dataSource.paginator = this.paginator;
+            this.dataSource.sort = this.sort;
+          }
+        });
       this.products.push({
         id: 1,
         name: 'Bujia',
         price: 200,
         stock: 40,
-        vehicles: []
+        vehicles: [
+          {
+            universalCode: 'AADDS',
+            brand: {
+              id: 1,
+              name: 'Hyundai',
+            },
+            line: {
+              id: 1,
+              name: 'Assent',
+            },
+            year: 2020,
+            brandId: 1,
+            lineId: 1,
+          },
+        ],
       });
       this.products.push({
         id: 2,
@@ -65,18 +88,25 @@ export class SalesProductDialogComponent implements OnInit {
         vehicles: [
           {
             universalCode: 'AADDSSkS',
-            brand: {},
-            line: {},
+            brand: {
+              id: 1,
+              name: 'Ford',
+            },
+            line: {
+              id: 1,
+              name: 'Escape',
+            },
             year: 2002,
             brandId: 1,
-            lineId: 1
-          }
+            lineId: 1,
+          },
         ],
-        stock: 4
+        stock: 4,
       });
       this.dataSource = new MatTableDataSource<ProductInterface>(this.products);
       this.dataSource.paginator = this.paginator;
       this.dataSource.sort = this.sort;
+      console.log(this.products);
     } catch (error) {
       this._DIALOG_SERVICE.errorMessage(
         error,
@@ -88,7 +118,9 @@ export class SalesProductDialogComponent implements OnInit {
 
   private getProductFabric(): void {
     try {
-      this.dataSourceFabric = new MatTableDataSource<ProductInterface>(this.productsFabric);
+      this.dataSourceFabric = new MatTableDataSource<ProductInterface>(
+        this.productsFabric
+      );
       this.dataSourceFabric.paginator = this.paginator;
     } catch (error) {
       this._DIALOG_SERVICE.errorMessage(
@@ -105,18 +137,107 @@ export class SalesProductDialogComponent implements OnInit {
     this.dataSourceFabric.filter = filterValue.trim().toLowerCase();
   }
 
-  applyFilterVehicle(event: Event) {
-    const filterValue = (event.target as HTMLSelectElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-    this.dataSourceFabric.filter = filterValue.trim().toLowerCase();
+  applyDataFilter(data: ProductInterface[]) {
+    this.dataSource = new MatTableDataSource<ProductInterface>(data);
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyDataFilterFabric(data: ProductInterface[]) {
+    this.dataSourceFabric = new MatTableDataSource<ProductInterface>(data);
+    this.dataSourceFabric.paginator = this.paginator;
+    this.dataSourceFabric.sort = this.sort;
+  }
+  applyFilterVehicle(event: any) {
+    try {
+      if (event.value != null) {
+        let partFound: any[] = [];
+
+        this.products.forEach((part: ProductInterface) => {
+          let found = [];
+          part.vehicles.forEach((vehicle) => {
+            if (vehicle.universalCode === event.value.universalCode) {
+              found.push(vehicle);
+            }
+          });
+
+          if (found.length > 0) {
+            partFound.push(part);
+          }
+        });
+
+        this.applyDataFilter(partFound);
+      } else {
+        this.getProduct();
+      }
+    } catch (error) {
+      this._DIALOG_SERVICE.errorMessage(
+        error,
+        'Error',
+        'Error al filtrar por vehiculo'
+      );
+    }
+  }
+
+  applyFilterVehicleFactory(event: any) {
+    try {
+      if (event.value != null) {
+        let partFound: any[] = [];
+
+        this.productsFabric.forEach((part: ProductInterface) => {
+          let found = [];
+          part.vehicles.forEach((vehicle) => {
+            if (vehicle.universalCode === event.value.universalCode) {
+              found.push(vehicle);
+            }
+          });
+
+          if (found.length > 0) {
+            partFound.push(part);
+          }
+        });
+
+        this.applyDataFilterFabric(partFound);
+      } else {
+        this.getProductFabric();
+      }
+    } catch (error) {
+      this._DIALOG_SERVICE.errorMessage(
+        error,
+        'Error',
+        'Error al filtrar por vehiculo'
+      );
+    }
   }
 }
 
 const ELEMENT_DATA_VEHICLES = [
   {
     universalCode: 'AADDSSkS',
-    brand: 'Ford',
-    line: 'Escape',
-    year: 2002
-  }
-]
+    brand: {
+      id: 1,
+      name: 'Ford',
+    },
+    line: {
+      id: 1,
+      name: 'Escape',
+    },
+    year: 2002,
+    brandId: 1,
+    lineId: 1,
+  },
+  {
+    universalCode: 'AADDS',
+    brand: {
+      id: 1,
+      name: 'Hyundai',
+    },
+    line: {
+      id: 1,
+      name: 'Assent',
+    },
+    year: 2020,
+    brandId: 1,
+    lineId: 1,
+  },
+];
